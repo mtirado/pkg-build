11bd2f65454786b77ced2f98da82e45508961e59
mtirado <mtirado418@gmail.com>
2016-06-16 10:31:12 -0400

    update example build script

--- package/pkg-compile.sh
+++ package/pkg-compile.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-# assumes untarred directory is same as filename without .tar.* extension.
+# example build script for qemu i386 virtual machine
+# assumes untarred directory is in cwd, and same as tar name without .tar.* extension.
+
 set -e
 umask 022
-read -r LINE<wares
+
+read -r LINE<$PKGDIR/wares
 ARCHIVEDIR=${LINE%.tar.*}
+if [ ! -d "$ARCHIVEDIR" ]; then
+	echo "archive dir is missing: $ARCHIVEDIR"
+	exit -1
+fi
+
 cd $ARCHIVEDIR
-echo "$ARCHIVEDIR/make -j$JOBS"
+
+./configure 						\
+		--prefix=$PKGDISTDIR			\
+		--target-list=i386-softmmu
+#		--enable-seccomp			\
 
 make -j$JOBS
+make install
