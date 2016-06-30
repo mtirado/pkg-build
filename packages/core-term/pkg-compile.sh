38b801e4ff48e5b948dcb6196df9bcfb493f58ed
mtirado <mtirado418@gmail.com>
2016-06-30 06:55:40 -0400

    fix ncurses installation

--- packages/core-term/pkg-compile.sh
+++ packages/core-term/pkg-compile.sh
@@ -3,7 +3,8 @@
 set -e
 CWD=$(pwd)
 IFS=' '
-
+# assumes ncurses install prefix will be /usr
+CURSES_PREFIX=/usr
 export PKG_CONFIG_PATH="/usr/lib/pkgconfig"
 while read LINE ;do
 	cd $CWD
@@ -20,21 +21,24 @@ while read LINE ;do
 	echo "pkg dir $PKGDIR"
 	case "$ARCHIVEDIR" in
 		ncurses*)
-			./configure 			\
-				--prefix=$PKGROOT	\
-				--with-shared		\
-				--without-debug
-			# procps-ng build system is a bit flaky without pkg-config
-			export CFLAGS="-I$PKGROOT/include -I$PKGROOT/include/ncurses"
-			export LDFLAGS="-L$PKGROOT/lib -L$PKGROOT/lib/ncurses"
-			export NCURSES_LIBS="-L$PKGROOT/lib -lncurses"
-			export NCURSES_CFLAGS="-I$PKGROOT/include/ncurses"
+			DESTDIR=$PKGROOT				\
+			./configure 					\
+				--prefix=$CURSES_PREFIX			\
+				--with-shared				\
+				--without-debug				\
+				--without-normal			\
+				#--enable-pc-files
+
+			export CPPFLAGS="-I$PKGROOT/include -I$PKGROOT/include"
+			export LDFLAGS="-L$PKGROOT/lib -L$PKGROOT/lib"
+
 		;;
 		ex-*)
 			sed -i "s|DESTDIR.*=|DESTDIR = $PKGROOT|" Makefile
 			sed -i "s|/usr/ucb|/usr/bin|" Makefile
 			sed -i "s|/usr/local|/|" Makefile
 			sed -i "s|= termlib|= ncurses|" Makefile
+
 		;;
 		procps*)
 			./configure 			\
@@ -44,12 +48,21 @@ while read LINE ;do
 		#*)
 		#	./configure 			\
 		#		--prefix=$PKGROOT
+
 	esac
 
 	make -j$JOBS
 	make install
 
-	#post install case can go here if needed
+	case "$ARCHIVEDIR" in
+		# move files from /usr to / in pkgroot maybe ncurses should be
+		# it's own package instead of hacking around hacks
+		ncurses*)
+			cp -fva $PKGROOT/$CURSES_PREFIX/* $PKGROOT/
+			rm -rfv $PKGROOT/$CURSES_PREFIX
+		;;
+	esac
+
 	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$PKGROOT/lib/pkgconfig"
 
 done < $PKGDIR/wares
