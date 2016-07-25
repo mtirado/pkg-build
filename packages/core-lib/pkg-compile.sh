ede57f4c49084c86981c87304010469c46cbb71a
mtirado <mtirado418@gmail.com>
2016-07-25 08:22:02 -0400

    update core-lib for multipass

--- packages/core-lib/pkg-compile.sh
+++ packages/core-lib/pkg-compile.sh
@@ -1,69 +1,61 @@
 #!/bin/sh
-# assumes untarred directory is same as filename without .tar.* extension.
 set -e
-CWD=$(pwd)
-IFS=' '
+case "$PKGARCHIVE" in
+	p11-kit*)
+		./configure 			\
+			--prefix=$PKGROOT	\
+			--without-libffi	\
+			--with-trust-paths=/etc/pkcs11
+		mkdir -pv $PKGROOT/etc/pkcs11
+	;;
+	#libiconv*)
+	#	# patch glibc C11 error
+	#	patch -p1 < $PKGDIR/1-avoid-gets-error.patch
+	#	./configure 			\
+	#		--prefix=$PKGROOT
+	#;;
+	# TODO move this to a core GUI package, and test out these options
+	freetype*)
+		DEFSTR="FT_CONFIG_OPTION_PIC"
+		sed -i "s|.*$DEFSTR.*|#define $DEFSTR|" devel/ftoption.h
+		DEFSTR="FT_CONFIG_OPTION_SUBPIXEL_RENDERING"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
+		DEFSTR="FT_CONFIG_OPTION_ADOBE_GLYPH_LIST"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
+		DEFSTR="FT_CONFIG_OPTION_MAC_FONTS"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
+		DEFSTR="FT_CONFIG_OPTION_INCREMENTAL"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
+		DEFSTR="FT_CONFIG_OPTION_USE_HARFBUZZ"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
 
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/lib/pkgconfig"
-while read LINE ;do
-	cd $CWD
-	PKGROOT=$PKGDISTDIR/$(echo $LINE | cut -d " " -f 1)
-	ARCHIVEDIR=$(echo $LINE | cut -d " " -f 2)
-	ARCHIVEDIR=${ARCHIVEDIR%.tar.*}
-	if [ ! -d "$ARCHIVEDIR" ]; then
-		echo "archive dir $ARCHIVEDIR is missing"
-		exit -1
-	fi
-	# skip completed builds
-	if [ -e "$ARCHIVEDIR/.pkg-built" ]; then
-		continue
-	fi
+		DEFSTR="TT_CONFIG_OPTION_BYTECODE_INTERPRETER"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
+		DEFSTR="TT_CONFIG_OPTION_SUBPIXEL_HINTING"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
+		DEFSTR="TT_CONFIG_OPTION_GX_VAR_SUPPORT"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
+		DEFSTR="TT_CONFIG_OPTION_BDF"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
+		DEFSTR="TT_CONFIG_OPTION_NO_MM_SUPPORT"
+		sed -i "s|#undef.*$DEFSTR.*|#define $DEFSTR|" devel/ftoption.h
 
-	cd $ARCHIVEDIR
-	echo "archive dir $ARCHIVEDIR"
-	echo "pkg dir $PKGDIR"
-	case "$ARCHIVEDIR" in
-		#case pkgname*)
-		#;;
-		libgpg-error*)
-			./configure 			\
-				--prefix=$PKGROOT
-			export PKG_LIBGPGERROR=$PKGROOT
-		;;
-		libgcrypt*)
-			if [ -z "$PKG_LIBGPGERROR" ]; then
-				echo "PKG_LIBGPGERROR dir is not set, use export"
-				echo "and try again, or rm libgpg-error source dir"
-				exit -1
-			fi
-			GPG_ERROR_CONFIG=$PKG_LIBGPGERROR/bin/gpg-error-config \
-			GPG_ERROR_LIBS=$PKG_LIBGPGERROR/lib                    \
-			GPG_ERROR_CFLAGS=$PKG_LIBGPGERROR/include	       \
-			./configure 				               \
-				--prefix=$PKGROOT
-		;;
-		p11-kit*)
-			./configure 			\
-				--prefix=$PKGROOT	\
-				--with-trust-paths=/etc/pkcs11
-			mkdir -pv $PKGROOT/etc/pkcs11
-		;;
-		libiconv*)
-			# patch glibc C11 error
-			patch -p1 < $PKGDIR/1-avoid-gets-error.patch
-			./configure 			\
-				--prefix=$PKGROOT
-		;;
-		*)
-			./configure 			\
-				--prefix=$PKGROOT
-	esac
+		DEFSTR="AF_CONFIG_OPTION_CJK"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
+		DEFSTR="AF_CONFIG_OPTION_INDIC"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
+		DEFSTR="AF_CONFIG_OPTION_USE_WARPER"
+		sed -i "s|#define.*$DEFSTR.*|#undef $DEFSTR|" devel/ftoption.h
 
-	make -j$JOBS
-	make install
+		./configure 			\
+			--prefix=$PKGROOT
+	;;
+	*)
+		./configure 			\
+			--prefix=$PKGROOT
+esac
 
-	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$PKGROOT/lib/pkgconfig"
-	touch ".pkg-built"
+make -j$JOBS
+make install
 
-done < $PKGDIR/wares
 
