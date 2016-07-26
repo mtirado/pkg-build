212a5429c63ea3078ae0aa02cd05df1a6c84cae7
mtirado <mtirado418@gmail.com>
2016-07-26 00:47:44 -0400

    update core-term for multipass build

--- packages/core-term/pkg-compile.sh
+++ packages/core-term/pkg-compile.sh
@@ -1,81 +1,51 @@
 #!/bin/sh
-# assumes untarred directory is same as filename without .tar.* extension.
 set -e
-CWD=$(pwd)
-IFS=' '
-# assumes ncurses install prefix will be /usr
+
 CURSES_PREFIX=/usr
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/lib/pkgconfig"
-while read LINE ;do
-	cd $CWD
-	PKGROOT=$PKGDISTDIR/$(echo $LINE | cut -d " " -f 1)
-	ARCHIVEDIR=$(echo $LINE | cut -d " " -f 2)
-	ARCHIVEDIR=${ARCHIVEDIR%.tar.*}
-	if [ ! -d "$ARCHIVEDIR" ]; then
-		echo "archive dir $ARCHIVEDIR is missing"
-		exit -1
-	fi
-	# skip completed builds
-	if [ -e "$ARCHIVEDIR/.pkg-built" ]; then
-		continue
-	fi
+case "$PKGARCHIVE" in
+	ncurses*)
+		DESTDIR=$PKGROOT				\
+		./configure 					\
+			--prefix=$CURSES_PREFIX			\
+			--with-shared				\
+			--without-debug				\
+			--without-normal			\
+			--enable-pc-files
 
-	cd $ARCHIVEDIR
-	echo "archive dir $ARCHIVEDIR"
-	echo "pkg dir $PKGDIR"
-	case "$ARCHIVEDIR" in
-		ncurses*)
-			DESTDIR=$PKGROOT				\
-			./configure 					\
-				--prefix=$CURSES_PREFIX			\
-				--with-shared				\
-				--without-debug				\
-				--without-normal			\
-				#--enable-pc-files
+	;;
+	ex-*)
+		sed -i "s|DESTDIR.*=|DESTDIR = $PKGROOT|" Makefile
+		sed -i "s|/usr/ucb|/usr/bin|" Makefile
+		sed -i "s|/usr/local|/|" Makefile
+		sed -i "s|= termlib|= ncurses|" Makefile
 
-			export CPPFLAGS="-I$PKGROOT/include -I$PKGROOT/include"
-			export LDFLAGS="-L$PKGROOT/lib -L$PKGROOT/lib"
+	;;
+	procps*)
+		./configure 			\
+			--prefix=$PKGROOT	\
+			--disable-kill
+	;;
+	# XXX --disable-nls is used because i don't need this.
+	# sorry rest of world  :P
+	kbd-1.15*)
+		./configure 			\
+			--prefix=$PKGROOT	\
+			--disable-nls
+	;;
+	*)
+		./configure 			\
+			--prefix=$PKGROOT
 
-		;;
-		ex-*)
-			sed -i "s|DESTDIR.*=|DESTDIR = $PKGROOT|" Makefile
-			sed -i "s|/usr/ucb|/usr/bin|" Makefile
-			sed -i "s|/usr/local|/|" Makefile
-			sed -i "s|= termlib|= ncurses|" Makefile
+esac
 
-		;;
-		procps*)
-			./configure 			\
-				--prefix=$PKGROOT	\
-				--disable-kill
-		;;
-		# XXX --disable-nls is used for kbd because
-		# i don't need this.  sorry, rest of world  :P
-		kbd-1.15*)
-			./configure 			\
-				--prefix=$PKGROOT	\
-				--disable-nls
-		;;
-		#*)
-		#	./configure 			\
-		#		--prefix=$PKGROOT
+make -j$JOBS
+make install
 
-	esac
+case "$PKGARCHIVE" in
+	ncurses*)
+		mv -fv $PKGROOT/$CURSES_PREFIX/* $PKGROOT/
+		rm -rfv $PKGROOT/$CURSES_PREFIX
+	;;
+esac
 
-	make -j$JOBS
-	make install
-
-	case "$ARCHIVEDIR" in
-		# move files from /usr to / in pkgroot, maybe ncurses should be
-		# it's own package instead of hacking around hacks
-		ncurses*)
-			cp -fva $PKGROOT/$CURSES_PREFIX/* $PKGROOT/
-			rm -rfv $PKGROOT/$CURSES_PREFIX
-		;;
-	esac
-
-	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$PKGROOT/lib/pkgconfig"
-	touch ".pkg-built"
-
-done < $PKGDIR/wares
 
