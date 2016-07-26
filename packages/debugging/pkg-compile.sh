7b40401ed4cfdb3803dc9cfad8f220431bab3e5b
mtirado <mtirado418@gmail.com>
2016-07-26 01:50:50 -0400

    update debugging build for multipass

--- packages/debugging/pkg-compile.sh
+++ packages/debugging/pkg-compile.sh
@@ -1,40 +1,14 @@
 #!/bin/sh
-# assumes untarred directory is same as filename without .tar.* extension.
 set -e
-CWD=$(pwd)
-IFS=' '
 
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/lib/pkgconfig"
-while read LINE ;do
-	cd $CWD
-	PKGROOT=$PKGDISTDIR/$(echo $LINE | cut -d " " -f 1)
-	ARCHIVEDIR=$(echo $LINE | cut -d " " -f 2)
-	ARCHIVEDIR=${ARCHIVEDIR%.tar.*}
-	if [ ! -d "$ARCHIVEDIR" ]; then
-		echo "archive dir $ARCHIVEDIR is missing"
-		exit -1
-	fi
-	# skip completed builds
-	if [ -e "$ARCHIVEDIR/.pkg-built" ]; then
-		continue
-	fi
+case "$PKGARCHIVE" in
+	#case pkgname*)
+	#;;
+	*)
+		./configure 			\
+			--prefix=$PKGROOT
+esac
 
-	cd $ARCHIVEDIR
-	echo "archive dir $ARCHIVEDIR"
-	echo "pkg dir $PKGDIR"
-	case "$ARCHIVEDIR" in
-		#case pkgname*)
-		#;;
-		*)
-			./configure 			\
-				--prefix=$PKGROOT
-	esac
-
-	make -j$JOBS
-	make install
-
-	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$PKGROOT/lib/pkgconfig"
-	touch ".pkg-built"
-
-done < $PKGDIR/wares
+make -j$JOBS
+make install
 
