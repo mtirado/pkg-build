a4e0670b53cd9727dc6cbc7f5e9f93be15de41d3
mtirado <mtirado418@gmail.com>
2017-07-01 05:35:39 +0000

    add desktop package group

---
+++ packages/desktop/pkg-compile.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+set -e
+source "$PKGINCLUDE"
+case "$PKGARCHIVE" in
+	xlunch*)
+
+		# save this package for the end of the build, it will read
+		# .desktop files in /usr/share/applications to generate
+		# the icon launchers automatically
+
+		# adjust conf file location, and fix some warnings
+		sed -i "s#\"/etc/xlunch#\"$PKGPREFIX/etc/xlunch#" xlunch.c
+		sed -i "s#Pixmap currentRootPixmap;#Pixmap currentRootPixmap = None;#" xlunch.c
+		sed -i "s#int cleanup()#void cleanup()#" xlunch.c
+
+		# copy in a modified genconf
+		cp -vf "$_PKG_DIR/genconf" ./extra/genconf
+
+		export CFLAGS="-O2 -s -pedantic -Wall -Wextra"
+		make
+		mkdir -vp "$PKGROOT/$PKGPREFIX/etc/xlunch"
+		mkdir -vp "$PKGROOT/$PKGPREFIX/share/xlunch"
+		mkdir -vp "$PKGROOT/$PKGPREFIX/bin"
+		cp -v  ./icons.conf "$PKGROOT/$PKGPREFIX/etc/xlunch"
+		cp -v  ./xlunch "$PKGROOT/$PKGPREFIX/bin"
+		cp -vr ./extra "$PKGROOT/$PKGPREFIX/share/xlunch"
+		chmod +x ./extra/genconf
+		cp -v ./extra/genconf "$PKGROOT/$PKGPREFIX/bin/xlunch-genconf"
+		make_tar_flatten_subdirs "$PKGROOT"
+		exit 0
+	;;
+	*)
+		./configure 			\
+			--prefix="$PKGPREFIX"
+	;;
+
+esac
+
+make "-j$JOBS"
+DESTDIR="$PKGROOT" make install
+
+case "$PKGARCHIVE" in
+	xfe*)
+		#setup default progs for xfe
+		echo "" >> "$PKGROOT/usr/share/xfe/xferc"
+		echo "# default helpers" >> "$PKGROOT/usr/share/xfe/xferc"
+		echo "[PROGS]" >> "$PKGROOT/usr/share/xfe/xferc"
+		echo "pdfviewer=mupdf-x11" >> "$PKGROOT/usr/share/xfe/xferc"
+		echo "imgviewer=xfi" >> "$PKGROOT/usr/share/xfe/xferc"
+		echo "videoplayer=gmplayer" >> "$PKGROOT/usr/share/xfe/xferc"
+		echo "audioplayer=gmplayer" >> "$PKGROOT/usr/share/xfe/xferc"
+		echo "imageditor=gimp-2.9" >> "$PKGROOT/usr/share/xfe/xferc"
+	;;
+esac
+make_tar_flatten_subdirs "$PKGROOT"
