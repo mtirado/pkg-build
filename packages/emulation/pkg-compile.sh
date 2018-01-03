5959dcbf3787594392d966c1ce8e78fb02aaaf00
mtirado <mtirado418@gmail.com>
2018-01-03 03:44:27 +0000

    add emulation package, qemu

---
+++ packages/emulation/pkg-compile.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+source "$PKGINCLUDE"
+case "$PKGARCHIVE" in
+	qemu*)
+		./configure 						\
+			--prefix="$PKGPREFIX"				\
+			--disable-vnc					\
+			--disable-bluez					\
+			--disable-capstone				\
+			--target-list="i386-softmmu, i386-linux-user, arm-softmmu, arm-linux-user, mips-softmmu, mips-linux-user, x86_64-softmmu, x86_64-linux-user"
+	;;
+	*)
+		./configure 			\
+			--prefix="$PKGPREFIX"
+	;;
+esac
+
+make "-j$JOBS"
+DESTDIR="$PKGROOT" make install
+make_tar_flatten_subdirs "$PKGROOT"
