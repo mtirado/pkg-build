d77dc5b3fa68db6509b9ee858853a1e0e6988391
mtirado <mtirado418@gmail.com>
2018-01-20 02:25:12 +0000

    add third parameter to override package contents, prune undesired packages

--- pkg-deliver.sh
+++ pkg-deliver.sh
@@ -5,9 +5,9 @@
 set -e
 #-----------------------------------------------------------------------------
 if [ -z "$1" ] || [ "$1" = "-h" ]; then
-	echo "usage: pkg-deliver <flockdir>"
-	echo "PKGINSTALL should point to the base directory"
-	echo "and not to any specific package prefix"
+	echo "usage: pkg-deliver <flockdir> [override-dir]"
+	echo "override-dir is optional, for customizing pkg contents on read-only media"
+	echo "PKGINSTALL should point to the target install directory"
 	exit -1
 fi
 # make absolute path
@@ -15,6 +15,15 @@ if [[ "$1" != /* ]]; then
 	FLOCKDIR="$(pwd)/$1"
 else
 	FLOCKDIR="$1"
+fi
+if [ "$2" != "" ]; then
+	OVERRIDE_DIR="$2"
+	if [ ! -d "$OVERRIDE_DIR" ]; then
+		echo "override-dir is not a dir: $OVERRIDE_DIR"
+		exit -1
+	fi
+else
+	OVERRIDE_DIR=""
 fi
 # install packages into this location.
 if [ "$PKGINSTALL" == "" ]; then
@@ -32,30 +41,36 @@ mkdir -p "$PKGTMP"
 
 #
 #-----------------------------------------------------------------------------
-
-
-cd "$FLOCKDIR"
+# only overrides the contents file
+if [ "$OVERRIDE_DIR" != "" ]; then
+	CONTENTS_DIR="$OVERRIDE_DIR"
+else
+	CONTENTS_DIR="$FLOCKDIR"
+fi
+cd "$CONTENTS_DIR"
 for ITEM in $(find . -mindepth 1 -maxdepth 1 -type f -printf '%f\n'); do
-	if [[ "$ITEM" != *.tar.* ]]; then
+	if [[ "$ITEM" != *.tar* ]]; then
 		#build pkgdist
 		PKGDIST="pkgdist-$ITEM"
+		echo ""
+		printf "extracting $ITEM: "
 		while read LINE ;do
 			PKGNAME="$(echo "$LINE" | cut -d " " -f 1)"
 			PKGTAR="$(echo "$LINE" | cut -d " " -f 2)"
 			_PKG_DIR="$PKGTMP/$PKGDIST/$PKGNAME"
 			if [ -e "$_PKG_DIR" ]; then
-				rm -rvf "./$_PKG_DIR"
+				rm -rvf "$_PKG_DIR"
 			fi
-			echo "extracing $PKGTAR"
+			printf "$PKGTAR "
 			mkdir -p "$_PKG_DIR"
 			cd "$_PKG_DIR"
 			tar xf "$FLOCKDIR/$PKGTAR"
-			cd "$FLOCKDIR"
+			cd "$CONTENTS_DIR"
 		done < "$ITEM"
+		echo ""
 		pkg-install.sh "$PKGTMP/$PKGDIST" "$ITEM"
 		rm -r "$_PKG_DIR"
 	fi
-	cd "$FLOCKDIR"
 done
 rm -r "$PKGTMP"
 echo "delivered."
